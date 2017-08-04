a03d764f738a9efa3fc2d2475d1081fc79a11d2c
gustavobonfim <bonfimoliveira@gmail.com>
2017-08-04 14:02:19 -0300

    finished CRUD operations

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'bootstrap-sass', '~> 3.3.6'
 gem 'jquery-rails'
 gem 'bootstrap-select-rails'
 gem "font-awesome-rails"
+gem 'simple_form'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
---
+++ app/controllers/invoices_controller.rb
@@ -0,0 +1,52 @@
+class InvoicesController < ApplicationController
+
+  def index
+    @invoices = Invoice.all
+  end
+
+  def new
+    @invoice = Invoice.new
+  end
+
+  def create
+    @invoice = Invoice.new(invoices_params)
+    if @invoice.save
+      flash[:success] = "Invoice successfully created"
+      redirect_to invoices_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @invoice = Invoice.find(params[:id])
+  end
+
+  def edit
+    @invoice = Invoice.find(params[:id])
+  end
+
+  def update
+    @invoice = Invoice.find(params[:id])
+    if @invoice.update(invoices_params)
+      flash[:success] = "Invoice successfully updated"
+      redirect_to invoices_path
+    else
+      flash[:danger] = "Invoice was not updated"
+      render 'edit'
+    end
+  end
+
+
+
+  def destroy
+    @invoice = Invoice.find(params[:id])
+    @invoice.destroy
+    redirect_to invoices_path
+  end
+
+  private
+  def invoices_params
+    params.require(:invoice).permit(:amount, :company, :contragent, :currency, :date)
+  end
+end
